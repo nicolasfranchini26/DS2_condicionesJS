ee5dc34fb101d174310b6c2c39285d6f0ca59e6b
isidora franchini <n.franchinic@gmail.com>
2024-09-16 21:19:25 -0300

    Desafío condiciones JS

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+
+//       1er Desafío
+
+let resultadoBorde = 0
+bordeImagen = document.querySelector('#bordeImagen')
+
+
+//        2do Desafío
+
+let totalStickers = 0
+resultadoStickers = document.querySelector('.resultado')
+
+//          3er Desafío
+
+let password = ""
+respuesta = document.querySelector('#resultado');
